9c8b4ebc340c79a6e39cce5c19eb8b301c3a4f37
Vince West <vince_west@apple.com>
2025-09-29 09:34:26 -0500

    update to new version of no-clown with dim

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -29,38 +29,21 @@ return {
     },
     {
         "aktersnurra/no-clown-fiesta.nvim",
-        name = "no-clown-fiesta",
         priority = 1000,
         config = function()
-            require("no-clown-fiesta").setup({
-                transparent = true, -- Enable this to disable the bg color
+            local plugin = require "no-clown-fiesta"
+            plugin.setup({
+                -- transparent = true,
+                -- theme = "dark",
+                theme = "dim",
+                -- theme = "light",
                 styles = {
-                    -- You can set any of the style values specified for `:h nvim_set_hl`
-                    comments = {},
-                    functions = {},
-                    keywords = {},
-                    lsp = {},
-                    match_paren = {},
-                    type = {},
-                    variables = {},
+                    type = { bold = true },
+                    lsp = { underline = false },
+                    match_paren = { underline = true },
                 },
             })
-            vim.cmd.colorscheme("no-clown-fiesta")
-
-            local bg_light = "#373B43"
-            local bg_dark = "#24272C"
-            local bg_cursor = bg_light
-
-            -- Make colorcolumn and statusline transparent after colorscheme loads
-            vim.api.nvim_set_hl(0, "ColorColumn", { bg = bg_light })
-            vim.api.nvim_set_hl(0, "StatusLine",  { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "LineNr", { fg = "#666666", bg = "NONE" })
-            vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#999999", bg = bg_cursor })
-            vim.api.nvim_set_hl(0, "VertSplit", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "WinSeparator", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "Visual", { bg = bg_dark })
-            vim.api.nvim_set_hl(0, "CursorLine", { bg = bg_cursor })
+            return plugin.load()
         end,
         lazy = false,
     },
