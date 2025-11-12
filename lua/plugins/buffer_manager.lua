c24b60fd471fcb15430ea13e376c743bee4bdff1
Vince West <vince_west@apple.com>
2025-11-12 10:39:11 -0600

    add buffer_manager plugin

---
+++ lua/plugins/buffer_manager.lua
@@ -0,0 +1,6 @@
+return {
+    "j-morano/buffer_manager.nvim",
+    keys = {
+        { "<leader>B", "<cmd>lua require('buffer_manager.ui').toggle_quick_menu()<cr>", desc = "Open buffer manager" }
+    }
+}
